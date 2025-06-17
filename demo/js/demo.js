e6b6dc76af03e4722bc8c4990992e5f57ca1a362
shaul mazor <shaul.mazor@gmail.com>
2025-06-17 18:41:36 +0300

    ...

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,6 +1,77 @@
+const hidePanels = function () {
+    $(".main-panel .panel").removeClass("show");
+}
 
+//-------------------------------------------
+
+const setSidebarClickEvents = function () {
+
+    $(".sidebar .item.students").click(() => {
+        showPanel("students");
+    })
+    $(".sidebar .item.lessons").click(() => {
+        showPanel("lessons");
+    })
+    $(".sidebar .item.tasks").click(() => {
+        showPanel("tasks");
+    })
+    $(".sidebar .item.website").click(() => {
+        showPanel("website");
+    })
+    $(".sidebar .item.reports").click(() => {
+        showPanel("reports");
+    })
+    $(".sidebar .item.settings").click(() => {
+        showPanel("settings");
+    })
+}
+
+//-------------------------------------------
+
+const showPanel = function (panelName) {
+    $(".main-panel .panel").removeClass("show");
+    $(`.sidebar .item`).removeClass("active");
+
+    if (panelName !== "") {
+        $(`.main-panel .panel.${panelName}`).addClass("show");
+        $(`.sidebar .item.${panelName}`).addClass("active");
+    }
+}
+
+//-------------------------------------------
+
+const setLessonMenuClickEvents = function () {
+
+    $(".panel.lessons .menu .upcoming").click(() => {
+        showLessonList("upconing");
+    })
+    $(".panel.lessons .menu .past").click(() => {
+        showLessonList("past");
+    })
+    $(".panel.lessons .menu .canceled").click(() => {
+        showLessonList("canceled");
+    })
+}
+
+//-------------------------------------------
+
+const showLessonList = function (listName) {
+
+}
+
+//-------------------------------------------
+
+const init = function () {
+    setSidebarClickEvents();
+    setLessonMenuClickEvents();
+
+    hidePanels();
+    showPanel("students");
+}
+
+//-------------------------------------------
 
 $(document).ready(function () {
-
+    init();
 });
 
